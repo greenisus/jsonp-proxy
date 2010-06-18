ace419e4adb63d4f2c84ef91258db12d3bb28756
Michael Mayo <greenisus@gmail.com>
2010-06-18 13:03:21 -0700

    method and body support

--- jsonp-proxy.rb
+++ jsonp-proxy.rb
@@ -8,13 +8,26 @@ require 'json'
 get '/' do
   headers = params['h'] ? JSON.parse(params['h']) : {}
   url = URI.parse(params['u'])
-  request = Net::HTTP::Get.new(url.path)
+  
+  if params['m'] == 'get'
+    request = Net::HTTP::Get.new(url.path)
+  elsif params['m'] == 'post'
+    request = Net::HTTP::Post.new(url.path)
+  elsif params['m'] == 'put'
+    request = Net::HTTP::Put.new(url.path)
+  elsif params['m'] == 'delete'
+    request = Net::HTTP::Delete.new(url.path)
+  elsif params['m'] == 'head'
+    request = Net::HTTP::Head.new(url.path)
+  else
+    request = Net::HTTP::Get.new(url.path)
+  end
+  
   headers.each do |key, value|
     request.add_field(key, value)
   end
   
-  # TODO: request method
-  # TODO: set body
+  request.body = params['b'] if params['b']
   
   http = Net::HTTP.new(url.host, url.port)
   http.use_ssl = true
